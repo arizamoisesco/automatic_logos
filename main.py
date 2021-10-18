cae72096d219a9c639155bba7c7cc78ec40d72f5
Moises Ariza <arizamoises@gmail.com>
2021-10-18 16:44:09 -0500

    add files

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def logo_in_image(fondo, logo):
     x, y = fondo.size
     print(x)
     fondo.paste(logo, (0,0), logo)
-    fondo.save("test.png", "png")
+    fondo.save("output/test.png", "png")
     fondo.show()
     
 
-img = Image.open(r"city.jpg")
-img2 = Image.open(r"facebook.png").convert("RGBA")
+img = Image.open(r"rubor.jpg")
+img2 = Image.open(r"Logo-Treasure-Store.png").convert("RGBA")
 img2 = redimencionar_image(img2)
 logo_in_image(img, img2)
