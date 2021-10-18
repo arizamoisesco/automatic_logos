ff8bbf6faf71637203f2736b8eee5b7c33859fd3
Moises Ariza <arizamoises@gmail.com>
2021-10-18 16:23:01 -0500

    funtion logo_in_image completed

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from PIL import Image
 
 def redimencionar_image(image):
     
-    redimencionada_img = image.resize((100, 100))
+    redimencionada_img = image.resize((300, 300))
     redimencionada_img.save("logo.png", "png")
+    return redimencionada_img
 
-img = Image.open("city.jpg")
-redimencionar_image(img)
+def logo_in_image(fondo, logo):
+    x, y = fondo.size
+    print(x)
+    fondo.paste(logo, (0,0), logo)
+    fondo.save("test.png", "png")
+    fondo.show()
+    
+
+img = Image.open(r"city.jpg")
+img2 = Image.open(r"facebook.png").convert("RGBA")
+img2 = redimencionar_image(img2)
+logo_in_image(img, img2)
